08f7cd895faca4ad4953bcc99d11e25f74e66670
Jeremiah LaRocco <jeremiah_larocco@fastmail.com>
2019-11-05 22:46:16 -0700

    Initial broken implementation...

--- gl-book.lisp
+++ gl-book.lisp
@@ -37,12 +37,27 @@
 (def-error-callback error-callback (message)
   (format t "Error: ~a~%" message))
 
+(defparameter *vertex-shader*
+  "#version 330\
+in vec3 vp;
+void main() {
+    gl_Position = vec4 (vp, 1.0);
+}")
+
+(defparameter *fragment-shader*
+  "#version 330
+out vec4 frag_color;
+void main() {
+frag_color = vec4 (0.5, 0.0, 0.5, 1.0);
+}")
+
 (defun render-scene ()
   (gl:enable :line-smooth
              :polygon-smooth
              :cull-face
              :depth-test :depth-clamp
              :blend)
+  (gl:depth-func :less)
   (gl:depth-range -10.1 10.1)
   (gl:blend-func :one :one-minus-src-alpha)
   (gl:clear-color 0.0 0.0 0.0 1.0)
@@ -63,37 +78,62 @@
                            :context-version-major 3
                            :context-version-minor 3
                            :opengl-forward-compat t
+                           :samples 4
                            :resizable t)
-        (setf %gl:*gl-get-proc-address* #'get-proc-address)
+        (format t "Renderer: ~a~%Version ~a~%" (gl:get-string :renderer) (gl:get-string :version))
+        ;; (setf %gl:*gl-get-proc-address* #'get-proc-address)
         (set-key-callback 'quit-on-escape)
         (set-error-callback 'error-callback)
         (set-mouse-button-callback 'mouse-handler)
         (set-scroll-callback 'scroll-handler)
 
-        (let ((vaos (gl:gen-vertex-arrays 1))
-              (buffers (gl:gen-buffers 1))
-              (num-vertices 6)
-              (vertices = (make-array (list num-vertices 2)
-                                      :element-type 'single-float
-                                      :initial-contents '(;; Triangle 1
-                                                          (-0.9 -0.9)
-                                                          (0.85 -0.9)
-                                                          (-0.9 0.85)
-                                                          ;; Triangle 2
-                                                          (0.9 -0.85)
-                                                          (0.9 0.9)
-                                                          (-0.85 0.9)))))
-              (gl:bind-vertex-array (car vaos))
-              (gl:bind-buffer :array-buffer (car buffers))
-              (gl:buffer-data :array-buffer :static-draw gl-vertices))
-        (gl:clear-color 0 0 0 1.0)
-        
-        ;; The event loop
-        (loop until (window-should-close-p)
-           do
-             (render-scene)
-           do (swap-buffers)
-           do (poll-events))))))
+        (gl:enable :line-smooth
+                   :polygon-smooth
+                   :depth-test)
+        (gl:depth-func :less)
+
+        (let ((points '(0.0f0 0.5f0 0.0f0
+                        0.5f0 -0.5f0 0.0f0
+                        -0.5f0 -0.5f0 0.0f0))
+              (vbos (gl:gen-buffers 1))
+              (vaos (gl:gen-vertex-arrays 1))
+              (gl-array (gl:alloc-gl-array :unsigned-short 6))
+              (vs (gl:create-shader :vertex-shader))
+              (fs (gl:create-shader :fragment-shader))
+              (shader-program (gl:create-program)))
+          (loop
+             for val in points
+             for i from 0
+             do (setf (gl:glaref gl-array i) val))
+          (gl:bind-buffer :array-buffer (car vbos)) 
+         (gl:buffer-data :element-array-buffer :static-draw gl-array)
+          (gl:free-gl-array gl-array)
+
+          (gl:bind-vertex-array (car vaos))
+          (gl:enable-vertex-attrib-array 0)
+          (gl:bind-buffer :array-buffer (car vbos))
+          (gl:vertex-attrib-pointer 0 3 :float :false 0 (cffi:null-pointer))
+
+          (gl:shader-source vs *vertex-shader*)
+          (gl:compile-shader vs)
+
+          (gl:shader-source fs *fragment-shader*)
+          (gl:compile-shader fs)
+
+          (gl:attach-shader shader-program fs)
+          (gl:attach-shader shader-program vs)
+          (gl:link-program shader-program)
+
+          (gl:clear-color 0.2f0 0.2f0 0.2f0 1.0)
+          (loop
+             until (window-should-close-p)
+             do
+               (gl:clear :color-buffer :depth-buffer)
+               (gl:use-program shader-program)
+               (gl:bind-vertex-array (car vaos))
+               (gl:draw-arrays :triangles 0 3)
+             do (swap-buffers)
+             do (poll-events)))))))
 
 (defun show-viewer (&optional (in-thread nil))
   (if in-thread
